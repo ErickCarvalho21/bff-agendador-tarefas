b2d097c7aa8d399fdcb3aafc54f771d80cae390d
Erick <erick.javaspringcount@gmai.com>
2025-10-08 13:55:07 -0300

    adicionado cron

---
+++ src/main/java/com/javanauta/bffagendadortarefas/business/CronService.java
@@ -0,0 +1,67 @@
+package com.javanauta.bffagendadortarefas.business;
+
+import com.javanauta.bffagendadortarefas.business.dto.in.LoginRequestDTO;
+import com.javanauta.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import com.javanauta.bffagendadortarefas.business.enums.StatusNotificacaoEnum;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CronService {
+
+    private final TarefasService tarefasService;
+    private final EmailService emailService;
+    private  final UsuarioService usuarioService;
+
+    @Value("${usuario.email}")
+    private String email;
+
+    @Value("${usuario.senha}")
+    private String senha;
+
+    @Scheduled(cron = "${cron.horario}")
+    public void buscaTarefasProximaHora(){
+        String token = login(converterParaRequestDTO());
+        log.info("Iniciado a busca de Tarefas");
+        LocalDateTime horaAtual = LocalDateTime.now(); //LocalDateTime.now (hora atual) plus
+        LocalDateTime horaFutura = LocalDateTime.now().plusHours(1);
+        //Qualquer Tarefa que fique entre hora atual - e a hora futura +1
+        // Se agora é 22 - qualquer entre 22h e 23h
+        //se é 22h - qualquer tarefra entre 23h e 23h05 -- antes
+        List<TarefasDTOResponse> listaTarefas = tarefasService.buscarTarefasAgendadasPorPeriodo(horaAtual, horaFutura, token);
+        log.info("Tarefas encontradas" + listaTarefas);
+
+        listaTarefas.forEach(tarefa -> {
+            emailService.enviaEmail(tarefa);
+            log.info("Email enviado para o usuario" + tarefa.getEmailUsuario());
+            tarefasService.alteraStatus(StatusNotificacaoEnum.NOTIFICADO, tarefa.getId(),
+                    token);
+        });
+        log.info("Finalizado a busca e notificaçao de Tarefas");
+
+    }
+
+    public String login(LoginRequestDTO dto){
+        return usuarioService.LoginUsuario(dto);
+    }
+
+    public LoginRequestDTO converterParaRequestDTO(){
+        return LoginRequestDTO.builder()
+                .email(email)
+                .senha(senha)
+
+                .build();
+    }
+
+
+
+}
--- src/main/java/com/javanauta/bffagendadortarefas/infrastructure/client/EmailClient.java
+++ src/main/java/com/javanauta/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 public interface EmailClient {
 
-
+    @PostMapping
     void enviarEmail(@RequestBody TarefasDTOResponse dto);
 
 }
